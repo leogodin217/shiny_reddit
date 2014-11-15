59684b381c9253333023b539e045d746652cdc2e
Leo Godin <leogodin217@gmail.com>
2014-11-15 16:35:29 -0700

    Connects to and validates subreddits

--- server.R
+++ server.R
@@ -8,10 +8,18 @@
 library(shiny)
 
 shinyServer(function(input, output) {
+  
+  subreddit_url = reactive(paste("http://reddit.com/r/", input$subreddit_name, ".json", sep="")) 
+  output$subreddit_url = renderText(subreddit_url())
 
-  output$distPlot <- renderPlot({
+ valid_subreddit = function(subreddit) {
+   # If no children, then it's not a valid subreddit
+   ifelse(length(subreddit()[[2]]$children) == 0, FALSE, TRUE) 
+ }
 
-
-  })
-
+ # Get the data and make sure it's valid
+ subreddit = reactive(fromJSON(file=subreddit_url()))
+ output$valid_subreddit = reactive(valid_subreddit(subreddit))
+ 
+ 
 })
--- ui.R
+++ ui.R
@@ -11,8 +11,21 @@ shinyUI(fluidPage(
 
   # Application title
   titlePanel("Shiny Reddit"),
+  sidebarPanel(
+    textInput("subreddit_name", "Which subreddit do you want to analyze", "patriots"),
+    submitButton("Analyze Subreddit")
+  ),
 
   mainPanel(
-    
+    textOutput("subreddit_url"),
+    textOutput("valid_subreddit"),
+    conditionalPanel(
+      condition = "output.valid_subreddit == 0",
+      h2("Not a valid subreddit")
+    ),
+    conditionalPanel(
+      condition = "output.valid_subreddit == 1",
+      h2("Valid subreddit")
     )
+  )
 ))
